a7b11bb134b0f2e2eab87bb20a90a4288426565c
NirmalGundlapelly <nirmalgoud2103@gmail.com>
2023-01-30 17:38:25 +0000

    80%/100%

---
+++ src/components/LogoutPopup/index.js
@@ -0,0 +1,52 @@
+import {withRouter} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import NxtWatchContext from '../../context/NxtWatchContext'
+
+import {
+  StyledPopup,
+  LogoutPopupBgContainer,
+  LogoutQuestion,
+  CancelButton,
+  ConfirmButton,
+  LogoutButton,
+} from './styledComponents'
+
+const LogoutPopup = props => (
+  <NxtWatchContext.Consumer>
+    {value => {
+      const {isDarkTheme} = value
+
+      // On Click Logout Button
+      const onClickConfirmLogout = () => {
+        Cookies.remove('jwt_token')
+        const {history} = props
+        history.replace('/login')
+      }
+      return (
+        <StyledPopup
+          isDarkTheme={isDarkTheme}
+          trigger={
+            <LogoutButton isDarkTheme={isDarkTheme}>Logout</LogoutButton>
+          }
+          modal
+        >
+          {close => (
+            <LogoutPopupBgContainer isDarkTheme={isDarkTheme}>
+              <LogoutQuestion isDarkTheme={isDarkTheme}>
+                Are you sure, you want to logout?
+              </LogoutQuestion>
+              <CancelButton type="button" onClick={close}>
+                Cancel
+              </CancelButton>
+              <ConfirmButton type="button" onClick={onClickConfirmLogout}>
+                Confirm
+              </ConfirmButton>
+            </LogoutPopupBgContainer>
+          )}
+        </StyledPopup>
+      )
+    }}
+  </NxtWatchContext.Consumer>
+)
+
+export default withRouter(LogoutPopup)
